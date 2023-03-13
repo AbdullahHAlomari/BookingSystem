5bd85eebe278ec100e5bd94958bb7318080d12d4
Ali <alipro.ig7@gmail.com>
2023-03-13 16:30:55 +0300

    update signup page

--- client/front-app/src/components/Auth/Login.tsx
+++ client/front-app/src/components/Auth/Login.tsx
@@ -13,48 +13,36 @@ import {
   Text,
   useColorModeValue,
   Link,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
-export default function SignupCard() {
+export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            SIGN-UP
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            LOG-IN
           </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-          Enjoy booking tickets with ease
-                      </Text>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Enjoy booking tickets with ease
+          </Text>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id="firstName" isRequired>
-                  <FormLabel>First Name</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName" isRequired> 
-                  <FormLabel>Last Name</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-            </HStack>
             <FormControl id="email" isRequired>
               <FormLabel>Email address</FormLabel>
               <Input type="email" />
@@ -62,13 +50,14 @@ export default function SignupCard() {
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
+                <Input type={showPassword ? "text" : "password"} />
+                <InputRightElement h={"full"}>
                   <Button
-                    variant={'ghost'}
+                    variant={"ghost"}
                     onClick={() =>
                       setShowPassword((showPassword) => !showPassword)
-                    }>
+                    }
+                  >
                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
                 </InputRightElement>
@@ -78,17 +67,19 @@ export default function SignupCard() {
               <Button
                 loadingText="Submitting"
                 size="lg"
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }}>
-                SIGN-UP
+                  bg: "blue.500",
+                }}
+              >
+                LOGIN
               </Button>
             </Stack>
             <Stack pt={6}>
-              <Text align={'center'}>
-                Already a user? <Link color={'blue.400'}>Sign in</Link>
+              <Text align={"center"}>
+                You don't have an account?{" "}
+                <Link color={"blue.400"}>Sign up</Link>
               </Text>
             </Stack>
           </Stack>
--- client/front-app/src/components/Auth/SignUp.tsx
+++ client/front-app/src/components/Auth/SignUp.tsx
@@ -1,60 +1,152 @@
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import {
-  Flex,
   Box,
+  Flex,
+  Heading,
+  VStack,
+  Text,
+  Input,
+  Button,
+  useToast,
+  HStack,
   FormControl,
   FormLabel,
-  Input,
   InputGroup,
-  HStack,
   InputRightElement,
   Stack,
-  Button,
-  Heading,
-  Text,
   useColorModeValue,
-  Link,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function LoginCard() {
+ const SignUp = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const toast = useToast();
+
+  const submitRegister = async () => {
+    try {
+      if (!password) {
+        toast({
+          title: `You passwords doesn't match`,
+          status: "error",
+          duration: 3000,
+          position: "top",
+        });
+        return;
+      }
+
+      const request = await fetch("http://localhost:3008/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ firstname, lastname, password, email }),
+      });
+
+      const data = await request.json();
+
+      if (request.status !== 201) {
+        toast({
+          title: data.message,
+          status: "error",
+          duration: 3000,
+          position: "top",
+        });
+        return;
+      }
+
+      toast({
+        title: data.message,
+        status: "success",
+        duration: 3000,
+        position: "top",
+      });
+      navigate("/login");
+    } catch (error) {
+      toast({
+        title: "Server Error !",
+        status: "error",
+        duration: 3000,
+        position: "top",
+      });
+    }
+  };
 
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            LOG-IN
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            SIGN-UP
           </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-          Enjoy booking tickets with ease
-                      </Text>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            Enjoy booking tickets with ease
+          </Text>
         </Stack>
         <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
           <Stack spacing={4}>
+            <HStack>
+              <Box>
+                <FormControl id="firstName" isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input
+                    type="text"
+                    onChange={(e) => setFirstname(e.target.value)}
+                    value={firstname}
+                  />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName" isRequired>
+                  <FormLabel>Last Name</FormLabel>
+                  <Input
+                    type="text"
+                    onChange={(e) => setLastname(e.target.value)}
+                    value={lastname}
+                  />
+                </FormControl>
+              </Box>
+            </HStack>
             <FormControl id="email" isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
             </FormControl>
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} />
-                <InputRightElement h={'full'}>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+                <InputRightElement h={"full"}>
                   <Button
-                    variant={'ghost'}
+                    variant={"ghost"}
                     onClick={() =>
                       setShowPassword((showPassword) => !showPassword)
-                    }>
+                    }
+                  >
                     {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                   </Button>
                 </InputRightElement>
@@ -64,17 +156,22 @@ export default function LoginCard() {
               <Button
                 loadingText="Submitting"
                 size="lg"
-                bg={'blue.400'}
-                color={'white'}
+                bg={"blue.400"}
+                color={"white"}
                 _hover={{
-                  bg: 'blue.500',
-                }}>
-                LOGIN 
+                  bg: "blue.500",
+                }}
+                onClick={submitRegister}
+              >
+                SIGN-UP
               </Button>
             </Stack>
             <Stack pt={6}>
-              <Text align={'center'}>
-              You don't have an account? <Link color={'blue.400'}>Sign up</Link>
+              <Text align={"center"}>
+                Already a user?{" "}
+                <Link color={"blue.400"} to={"/login"}>
+                  Sign in
+                </Link>
               </Text>
             </Stack>
           </Stack>
@@ -82,4 +179,6 @@ export default function LoginCard() {
       </Stack>
     </Flex>
   );
-}
+};
+
+export default SignUp;
